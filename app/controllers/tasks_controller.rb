f4f539b7600532bc271da7c1c964d14f9bdbe150
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-05-26 12:13:35 -0400

    added edit and update actions. created edit view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,13 +4,29 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = Task.new(tasks_params)
      if @task.save
        flash[:notice] = "Task was successfully created"
        redirect_to task_path(@task)
+
      else
        render 'new'
+     end
+   end
+
+   def update
+     @task = Task.find(params[:id])
+     if @task.update(tasks_params)
+       flash[:notice] = "Task was successfully updated"
+       redirect_to task_path(@task)
+
+     else
+       render 'edit'
      end
    end
 
