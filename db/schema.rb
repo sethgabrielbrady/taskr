e2d4d69134bd0ac636e81e3962449955fcd70959
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-05-26 14:31:26 -0400

    created user task association and updated controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524164945) do
+ActiveRecord::Schema.define(version: 20170526181605) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
